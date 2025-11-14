b0bef40f0bc74fdc89ba6b83a2181a1601a99552
IdotEXE-313 <mcpegamerxx1@gmail.com>
2025-11-14 14:42:03 +0000

    use the main.py file

--- q3attempt2/main.py
+++ q3attempt2/main.py
@@ -5,7 +5,9 @@ import scipy.integrate
 import matplotlib.pyplot as plt
 import sympy as sp
 
-#Question 1
+x = sp.symbols('x')
+
+#Define the recurrence relation to get the coefficents for the series solution
 def recurrence_relation(n:int,a0:int,a1:int) -> np.array:
     a = [a0,a1]
     for N in range(0, n-2):
@@ -13,6 +15,7 @@ def recurrence_relation(n:int,a0:int,a1:int) -> np.array:
         a.append(an)
     return np.array(a)
 
+#Generate the series solutions, where iterations are the number of terms we want
 def series(iterations:int) -> np.polynomial:
     
     a0_coeff = recurrence_relation(iterations, a0=1, a1=0)
@@ -23,20 +26,28 @@ def series(iterations:int) -> np.polynomial:
 
     return y0_poly, y1_poly
 
+#Define the exact solution that has been manually calculated to later plot
+def exact_solution():
+    return sp.lambdify(x, (1/4 + x/2 * sp.atan(x) + (1+x**2)/4 * (sp.atan(x))**2), modules=['numpy'])
+
+
 def sketch_series_solutions(y0_poly, y1_poly):
 
     x = np.linspace(-3,3,num=100)
 
+    solution = exact_solution()
+
     # plt.plot(x,y0_poly(x),label="y=1+x^2")
     # plt.plot(x, y1_poly(x), label="y=x+x^3/3-x^5/15+...")
-    plt.plot(x, y0_poly(x) + y1_poly(x), label="y=solution")
+    plt.plot(x, y0_poly(x) + y1_poly(x), label="y=solution") #General solution is the sum of these
+    plt.plot(x, solution(x), label="exact solution")
     plt.xlim(-3,3)
     plt.ylim(-10,10)
     plt.show()
 
 
 def main():
-    y0_poly, y1_poly = series(10)
+    y0_poly, y1_poly = series(100000) #Place holder, will calculate 8 terms (since a0, a1 already defined)
     sketch_series_solutions(y0_poly, y1_poly)
 
 if __name__ == "__main__":
